c63ace9f134d4c71219d0a71692ffbb0500e9251
Jerry Zhang <jerryzh168@gmail.com>
2025-06-24 10:59:26 -0700

    Add support for float8 activation for Int4GroupwisePreshuffleTensor
    Summary:
    Added basic op support like linear and bmm, we have both float8 and bf16 in the same Tensor
    because it's the same dtype, only difference is whether the activation is quantized or not. Although
    there is some differneces in implementation:
    
    bf16 activaton:
    * group_scale
    * group_zero
    
    fp8 activation
    * group_scale
    * row_scale
    
    Test Plan:
    python test/dtypes/test_float8_activation_int4_groupwise_preshuffle.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    stack-info: PR: https://github.com/pytorch/ao/pull/2437, branch: jerryzh168/stack/4

--- torchao/quantization/quant_api.py
+++ torchao/quantization/quant_api.py
@@ -2040,6 +2040,7 @@ class FbgemmConfig(AOBaseConfig):
        weight_dtype (torch.dtype): weight dtype of the kernel
        output_dtype (torch.dtype): output dtype of the kernel
        group_size (int): The group size for weight
+       preshuffle (bool): whether preshuffle the weights or not
     """
 
     input_dtype: torch.dtype
@@ -2067,7 +2068,9 @@ def _(module: torch.nn.Module, config: FbgemmConfig) -> torch.nn.Module:
     ):
         if config.preshuffle:
             weight = Int4GroupwisePreshuffleTensor.from_float(
-                module.weight, config.block_size
+                module.weight,
+                config.block_size,
+                activation_dtype="bf16",
             )
         else:
             weight = to_fbgemm_int4(
@@ -2077,6 +2080,20 @@ def _(module: torch.nn.Module, config: FbgemmConfig) -> torch.nn.Module:
         module.weight = torch.nn.Parameter(weight, requires_grad=False)
         module.extra_repr = types.MethodType(_linear_extra_repr, module)
         return module
+    if (
+        (config.input_dtype == e4m3_dtype)
+        and (config.weight_dtype == torch.int4)
+        and (config.output_dtype == torch.bfloat16)
+    ):
+        if config.preshuffle:
+            weight = Int4GroupwisePreshuffleTensor.from_float(
+                module.weight,
+                config.block_size,
+                activation_dtype="fp8",
+            )
+            module.weight = torch.nn.Parameter(weight, requires_grad=False)
+            module.extra_repr = types.MethodType(_linear_extra_repr, module)
+            return module
     elif (
         (config.input_dtype == e4m3_dtype)
         and (config.weight_dtype == e4m3_dtype)
